259ab9744a035c755c84ea378ffa8db9c8fa09bc
dplayz <dp@dplayzgames06.tk>
2022-03-19 16:37:42 +0800

    remove theme

--- themes/dplayzgames06WebCore/static/scripts/UTCtoLocalTime.js
+++
@@ -1,8 +0,0 @@
-var utcClock = document.getElementById('dateDisplay').innerText;
-const dateObject = new Date(utcClock)
-const localizedClock = dateObject.toLocaleString("en-US", {timeZoneName: "short"})
-window.onload = function() {
-    document.getElementById("dateDisplay").innerText = localizedClock ;
-  };
-console.log(utcClock); 
-console.log(localizedClock);
--- themes/dplayzgames06WebCore/static/scripts/UnixToLocalTime.js
+++
@@ -1,9 +0,0 @@
-var myClock = document.getElementById('dateDisplay').innerText;
-const milliseconds = myClock * 1000;
-const dateObject = new Date(milliseconds)
-const humanDateFormat = dateObject.toLocaleString("en-US", {timeZoneName: "short"})
-console.log(humanDateFormat);
-console.log (myClock);
-window.onload = function() {
-    document.getElementById("dateDisplay").innerText = humanDateFormat ;
-  };
